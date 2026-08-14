93263b8ca34d85d150bada24eef2e8cb2b9f65cf
Mehdipro <mahdi.shanaghi81@gmail.com>
2026-08-14 14:02:17 +0330

    some function change

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
   const pwd = document.getElementById('password');
   const eyesOpen = document.getElementById('eyes-open');
-  const eyesClosed = document.getElementById('eyes-closed');
+  //const eyesClosed = document.getElementById('eyes-closed');
   const hands = document.getElementById('hands');
 
   function peekAway() {
     eyesOpen.style.display = 'none';
-    eyesClosed.style.display = 'block';
+    //eyesClosed.style.display = 'block';
     hands.style.display = 'block';
   }
 
